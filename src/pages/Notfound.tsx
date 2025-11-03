8b21aaf474043c0fe2c2ee1d0c6f3271de9e2de9
Bongeka-Bhungane <angelabhungane@gmsil.com>
2025-11-03 09:20:33 +0200

    did the not found page and styled it using external css, now i will style the home page

--- src/pages/Notfound.tsx
+++ src/pages/Notfound.tsx
@@ -1,15 +1,12 @@
 import React from "react";
 import Button from "../compontents/Button";
+import folderImg from "../assets/images/folder.png"; 
 
 export default function Notfound() {
   return (
     <div className="notfound-page">
-      <h1>404 Page Not Found</h1>
-      <img
-        src="../assets/images/folder.png"
-        alt="error image"
-        className="error-image"
-      />
+      <h1 className="notfound-title">404 Page Not Found</h1>
+      <img src={folderImg} alt="error image" className="error-image" />
       <Button
         name="Go Back"
         color="#fff"
